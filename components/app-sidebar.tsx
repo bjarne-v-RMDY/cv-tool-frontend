061ab1a9c81846d3d46d3d46ab1372cd1e3ac4a8
Bjarne Verdonck <bjarne.verdonck@rmdy.be>
2025-10-08 16:44:40 +0200

    version bump

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -60,7 +60,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </div>
           <div className="flex flex-col gap-0.5 leading-none">
             <span className="font-medium">CV-Tool</span>
-            <span className="text-xs">v0.0.3</span>
+            <span className="text-xs">v0.1.0</span>
           </div>
         </div>
       </SidebarHeader>
